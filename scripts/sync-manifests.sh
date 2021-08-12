65442a5a38bf118c3e2d43c9a8a29efec9267dbe
Carlos Santana <csantana23@gmail.com>
2021-08-11 22:05:07 -0400

    update sync manifest script
    Signed-off-by: Carlos Santana <csantana23@gmail.com>

--- scripts/sync-manifests.sh
+++ scripts/sync-manifests.sh
@@ -17,23 +17,3 @@ do
     done
 
 done
-exit 0
-
-cp 0-bootstrap/argocd/single-cluster/2-services/argocd/instances/* 0-bootstrap/argocd/others/1-shared-cluster/cluster-1-cicd-dev-stage-prod/2-services/argocd/instances/
-cp 0-bootstrap/argocd/single-cluster/2-services/argocd/operators/* 0-bootstrap/argocd/others/1-shared-cluster/cluster-1-cicd-dev-stage-prod/2-services/argocd/operators/
-cp 0-bootstrap/argocd/single-cluster/2-services/argocd/instances/* 0-bootstrap/argocd/others/1-shared-cluster/cluster-n-prod/2-services/argocd/instances/
-cp 0-bootstrap/argocd/single-cluster/2-services/argocd/operators/* 0-bootstrap/argocd/others/1-shared-cluster/cluster-n-prod/2-services/argocd/operators/
-
-cp 0-bootstrap/argocd/single-cluster/2-services/argocd/instances/* 0-bootstrap/argocd/others/2-isolated-cluster/cluster-1-cicd-dev-stage/2-services/argocd/instances/
-cp 0-bootstrap/argocd/single-cluster/2-services/argocd/operators/* 0-bootstrap/argocd/others/2-isolated-cluster/cluster-1-cicd-dev-stage/2-services/argocd/operators/
-cp 0-bootstrap/argocd/single-cluster/2-services/argocd/instances/* 0-bootstrap/argocd/others/2-isolated-cluster/cluster-n-prod/2-services/argocd/instances/
-cp 0-bootstrap/argocd/single-cluster/2-services/argocd/operators/* 0-bootstrap/argocd/others/2-isolated-cluster/cluster-n-prod/2-services/argocd/operators/
-
-cp 0-bootstrap/argocd/single-cluster/2-services/argocd/instances/* 0-bootstrap/argocd/others/3-multi-cluster/cluster-1-cicd/2-services/argocd/instances/
-cp 0-bootstrap/argocd/single-cluster/2-services/argocd/operators/* 0-bootstrap/argocd/others/3-multi-cluster/cluster-1-cicd/2-services/argocd/operators/
-cp 0-bootstrap/argocd/single-cluster/2-services/argocd/instances/* 0-bootstrap/argocd/others/3-multi-cluster/cluster-2-dev/2-services/argocd/instances/
-cp 0-bootstrap/argocd/single-cluster/2-services/argocd/operators/* 0-bootstrap/argocd/others/3-multi-cluster/cluster-2-dev/2-services/argocd/operators/
-cp 0-bootstrap/argocd/single-cluster/2-services/argocd/instances/* 0-bootstrap/argocd/others/3-multi-cluster/cluster-3-stage/2-services/argocd/instances/
-cp 0-bootstrap/argocd/single-cluster/2-services/argocd/operators/* 0-bootstrap/argocd/others/3-multi-cluster/cluster-3-stage/2-services/argocd/operators/
-cp 0-bootstrap/argocd/single-cluster/2-services/argocd/instances/* 0-bootstrap/argocd/others/3-multi-cluster/cluster-n-prod/2-services/argocd/instances/
-cp 0-bootstrap/argocd/single-cluster/2-services/argocd/operators/* 0-bootstrap/argocd/others/3-multi-cluster/cluster-n-prod/2-services/argocd/operators/
